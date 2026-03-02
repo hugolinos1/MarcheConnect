7463e38f5dd0dbc60e0364fac9317f7887946092
Hugues R <hugues.rabier@gmail.com>
2026-03-02 20:24:56 +0000

    Try fixing this error: `Build Error: Parsing ecmascript source code fail

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -482,7 +482,7 @@ export default function AdminDashboard() {
                   size="sm"
                   className="absolute right-0 top-0 h-full px-3 py-2 hover:bg-transparent"
                   onClick={() => setShowAuthPass(!showAuthPass)}
-                />
+                >
                   {showAuthPass ? <EyeOff className="h-4 w-4 text-muted-foreground" /> : <Eye className="h-4 w-4 text-muted-foreground" />}
                 </Button>
               </div>
